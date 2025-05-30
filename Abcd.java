72a8735446fdc5c3eba835489692bdb1d1d373f1
Ayush-0729 <ayushrai2907@gmail.com>
2025-05-30 13:41:17 +0530

    Create Abcd.java

---
+++ Abcd.java
@@ -0,0 +1,4 @@
+Types of IT Infrastructure: 
+On-Premise Infrastructure: This involves hardware and software owned and managed within an organization's own facilities. 
+Cloud Infrastructure: This relies on third-party cloud providers to deliver IT services over the internet. 
+Hybrid Infrastructure: This combines elements of on-premise and cloud infrastructure. 
